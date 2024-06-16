b8b779981ac59df52a0e63a9f4d754c628427210
Abdiwali Maxamed <abdiwali.maxamed.hersi@gmail.com>
2024-06-16 23:02:59 +0200

    customize login templates with logo and social login buttons

--- bunadmin/urls.py
+++ bunadmin/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('users.urls')),
+    path('', include('users.urls'), name='users'),
     path('admin/', admin.site.urls),
     path('dashboard', include('dashboard.urls')),
 ]
